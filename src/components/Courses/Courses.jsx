f0d018340b834ef3a2dee56bd647145fd7e9b799
altravolf <altravolf@001.com>
2024-01-17 13:39:25 +0530

    PageHeading Overflow solved

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -49,32 +49,35 @@ function Courses() {
 
 
     return (
-        <div className="Courses py-3 mt-3 container">
 
+        <>
             {window.location.pathname === "/courses" ? <PageHeading headingText={"Popular Courses"} /> : <SectionHeading headingText={"Popular Courses"} />}
+            <div className="Courses py-3 container">
 
-            <div className="row justify-content-center px-3 px-md-0 mt-3">
+                <div className="row justify-content-center px-3 px-md-0">
 
-                {courses.map((el, i) => {
-                    return <div className="col-12 col-lg-4 col-md-6 my-3 cursor-pointer" data-aos="zoom-in-down" data-aos-out="zoom-in-up" key={i}>
-                        <div className="card h-100" >
-                            <img src={`/img/course-${i}.jpg`} className="card-img-top" alt="{i}" />
+                    {courses.map((el, i) => {
+                        return <div className="col-12 col-lg-4 col-md-6 my-3 cursor-pointer" data-aos="zoom-in-down" data-aos-out="zoom-in-up" key={i}>
+                            <div className="card h-100" >
+                                <img src={`/img/course-${i}.jpg`} className="card-img-top" alt="{i}" />
 
-                            <div className="card-body">
-                                <div className="d-flex justify-content-between flex-wrap mb-2 align-items-center">
-                                    <div className="card-type ">{el.courseType}</div>
-                                    <div className="card-price h5">{el.price}</div>
+                                <div className="card-body">
+                                    <div className="d-flex justify-content-between flex-wrap mb-2 align-items-center">
+                                        <div className="card-type ">{el.courseType}</div>
+                                        <div className="card-price h5">{el.price}</div>
+                                    </div>
+                                    <h5 className="card-title h5">{el.courseTitle}</h5>
+                                    <p className="card-text text-muted fw-normal small">{el.description}</p>
                                 </div>
-                                <h5 className="card-title h5">{el.courseTitle}</h5>
-                                <p className="card-text text-muted fw-normal small">{el.description}</p>
                             </div>
                         </div>
-                    </div>
-                })}
+                    })}
+
+                </div>
 
             </div>
+        </>
 
-        </div>
     );
 }
 
--- src/components/SectionHeading/SectionHeading.jsx
+++ src/components/SectionHeading/SectionHeading.jsx
@@ -2,7 +2,7 @@ import './SectionHeading.scss';
 
 function SectionHeading({ headingText }) {
     return (
-        <div className="SectionHeading">
+        <div className="SectionHeading mt-4">
             <h2 className="heading text-center display-5 heading fw-semibold">
                 <span className="title-underline">{headingText}</span>
             </h2>
