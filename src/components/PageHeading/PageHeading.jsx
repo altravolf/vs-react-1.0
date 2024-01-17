3ab61e2af76ff490169b2d469d0822995aaa4878
altravolf <altravolf@001.com>
2024-01-17 13:04:05 +0530

    PageHeading component created

---
+++ src/components/PageHeading/PageHeading.jsx
@@ -0,0 +1,13 @@
+import './PageHeading.scss';
+
+function PageHeading({ headingText }) {
+    return (
+        <div className="PageHeading">
+            <h2 className="heading text-center display-5 heading fw-semibold">
+                <span className="title-underline">{headingText}</span>
+            </h2>
+        </div>
+    );
+}
+
+export default PageHeading;
