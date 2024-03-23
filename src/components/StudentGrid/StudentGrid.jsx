c264c38fc2e15d1e02f54902d728762f132a4fdc
altravolf <altravolf@001.com>
2024-03-23 20:27:56 +0530

    student description, review data added

--- src/components/StudentGrid/StudentGrid.jsx
+++ src/components/StudentGrid/StudentGrid.jsx
@@ -2,19 +2,34 @@ import './StudentGrid.scss';
 
 const studentData = [
     {
-        name: "xyz"
+        name: "Renu Bala (MBBS)",
+        img: "renu_bala",
+        description: "Renu’s grasp of NEET material is exceptional, a testament to V.S. Sehrawat Institute’s rigorous training.",
+        review: "Thanks to V.S. Sehrawat Institute, my NEET prep was top-notch, and I’m confident about my medical college journey!"
     },
     {
-        name: "xyz"
+        name: "Vishnu (BAMS)",
+        img: "vishnu",
+        description: "Vishnu’s NEET scores reflect the high-quality coaching provided at V.S. Sehrawat Institute.",
+        review: "The NEET coaching at V.S. Sehrawat Institute was unparalleled. It truly honed my test-taking strategies."
     },
     {
-        name: "xyz"
+        name: "Sonam (BAMS)",
+        img: "sonam",
+        description: "Sonam’s NEET preparation at V.S. Sehrawat Institute has been thorough, leaving no stone unturned.",
+        review: "I owe my thorough NEET preparation to the dedicated faculty at V.S. Sehrawat Institute. Couldn’t have done it without them!"
     },
     {
-        name: "Sonam"
+        name: "Tinku (BAMS)",
+        img: "tinku",
+        description: "Tinku’s dedication to NEET studies at V.S. Sehrawat Institute is evident in his impressive performance.",
+        review: "The rigorous NEET practice sessions at V.S. Sehrawat Institute paid off. I’m ready for college!"
     },
     {
-        name: "Tinku"
+        name: "Nishant (BAMS)",
+        img: "nishant",
+        description: "Nishant’s NEET results are a clear indicator of the effective teaching strategies at V.S. Sehrawat Institute.",
+        review: "Achieving great NEET scores was a dream that V.S. Sehrawat Institute helped me realize. Grateful for the guidance!"
     },
 ]
 
@@ -26,11 +41,11 @@ function StudentGrid() {
 
                 {studentData.map((student, index) => (
                     <div className="col-12 col-lg-4 col-md-6 my-3 cursor-pointer aos-init aos-animate" data-aos="zoom-in-down" data-aos-out="zoom-in-up" key={index}>
-                        <div className="card h-100 ">
-                            <img src={`/img/student-${index + 1}.webp`} className="card-img-top object-fit-fill" alt="..." />
+                        <div className="card h-100 rounded-3 ">
+                            <img src={`/img/${student.img}.webp`} title={student.name} className="card-img-top object-fit-fill" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title text-center fs-4 text-capitalize">{student.name}</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <p className="card-text">{student.description}</p>
                             </div>
                         </div>
                     </div>
