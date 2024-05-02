a4ef886ebf05bae23c3383c36cf7c56e61a298bb
altravolf <altravolf@001.com>
2024-05-02 13:38:37 +0530

    contact us frontend ready

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,8 +8,8 @@ function ContactForm() {
 
     return (
         <div className="ContactForm container mt-5 ">
-            <div className="row flex-row-reverse align-items-center">
-                <div className=" col-6 ">
+            <div className="row flex-md-row-reverse align-items-center flex-column">
+                <div className=" col-12 col-md-6 ">
                     <h2 className="text-center">Quickly Reach Us</h2>
 
                     <div className=" d-flex justify-content-center align-items-center gap-5 mt-4">
@@ -40,28 +40,26 @@ function ContactForm() {
 
                 </div>
 
-                <div className="form shadow p-4 bg-white rounded-1 col-6">
+                <div className="form shadow p-4 bg-white rounded-1 col-11 col-md-6">
                     <form onSubmit={handleSubmit(onSubmit)}>
 
-                        {/* {window.location.pathname === "/contact" &&
-                            <h2 className="heading display-5 heading fw-semibold text-dark d-inline-block mx-auto position-relative start-50 translate-middle-x">Get in touch</h2>} */}
 
                         <h2 className="text-center">Get in touch</h2>
 
                         <div className="row">
-                            <div className="form-group my-2 col-4">
+                            <div className="form-group my-2 col-12 col-lg-4">
                                 <label className="form-label" htmlFor="fullName">Full Name:</label>
                                 <input className={"form-control" + (errors.fullName ? " error" : "")} placeholder={errors.fullName ? "This field is required" : "Naresh Trehan"} {...register("fullName", { required: true })} />
                             </div>
 
-                            <div className="form-group my-2 col-4">
+                            <div className="form-group my-2 col-12 col-lg-4">
                                 <label className="form-label" htmlFor="email">Email:</label>
                                 <input className={"form-control" + (errors.email ? " error" : "")}
                                     placeholder={errors.email ? "This field is required" : "abcde@example.com"}
                                     {...register("email", { required: true })} />
                             </div>
 
-                            <div className="form-group my-2 col-4">
+                            <div className="form-group my-2 col-12 col-lg-4">
                                 <label className="form-label" htmlFor="mobileNo">Mobile No.:</label>
                                 <input className={"form-control" + (errors.mobileNo ? " error" : "")} placeholder={errors.mobileNo ? "This field is required" : "8708331920"}  {...register("mobileNo", { required: true })} />
                             </div>
@@ -69,7 +67,7 @@ function ContactForm() {
 
                         <div className="form-group my-2">
                             <label className="form-label" htmlFor="message">Message:</label>
-                            <textarea className={"form-control" + (errors.message ? " error" : "")}
+                            <textarea rows="5" className={"form-control" + (errors.message ? " error" : "")}
                                 placeholder={errors.message ? "This field is required" : "Write your message..."}
                                 {...register("message", { required: true })} />
                         </div>
