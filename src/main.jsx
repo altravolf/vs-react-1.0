ef5836b5caa2ee0ffad86d4db28f3523cfecdea8
altravolf <altravolf001@gmail.com>
2024-06-08 20:54:36 +0530

    bootstrap CDN removed, npm bootstrap added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { SnackbarProvider } from "notistack";
 
+// src/main.jsx or wherever your main entry file is
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@popperjs/core/dist/umd/popper.min.js';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SnackbarProvider maxSnack={3}>
     <React.StrictMode>
