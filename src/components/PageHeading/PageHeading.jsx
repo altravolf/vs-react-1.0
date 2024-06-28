247a5e66b792a51039933d77d7189e5866c5f2ac
altravolf <altravolf001@gmail.com>
2024-06-28 16:23:09 +0530

    Page Heading fixed

--- src/components/PageHeading/PageHeading.jsx
+++ src/components/PageHeading/PageHeading.jsx
@@ -2,7 +2,7 @@ import './PageHeading.scss';
 
 function PageHeading({ headingText }) {
     return (
-        <div className="PageHeading overflow-hidden">
+        <div className="PageHeading ">
             <h2 className="heading text-center display-5 heading fw-semibold">
                 <span className="title-underline">{headingText}</span>
             </h2>
